bc0cbfbd8308bac3f8756e5b28178601048191cd
Hristiyan Genchev <genchev99@users.noreply.github.com>
2020-06-06 19:57:49 +0300

    users crud

--- homework_02/app.js
+++ homework_02/app.js
@@ -6,7 +6,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-
+const bodyParser = require('body-parser');
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
@@ -16,6 +16,7 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+app.use(bodyParser.json());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
--- homework_02/routes/users.js
+++ homework_02/routes/users.js
@@ -1,9 +1,43 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const user = require('../models/user');
 
 /* GET users listing. */
-router.get('/', (req, res, next) => {
-  res.send('respond with a resource');
+router.get('/', async (req, res) => {
+  const users = (await user.find()).map(user => user.toObject());
+
+  res.json({users});
+});
+
+/**
+ * Creates a new user in the database
+ */
+router.post('/', async (req, res) => {
+  try {
+    const {_id} = await user.create(req.body);
+    res.status(201).location(`/users/${_id}`).send();
+  } catch (e) {
+    res.json({error: e.toString()});
+  }
+});
+
+/**
+ * Deletes all users
+ */
+router.delete('/', async (req, res) => {
+  try {
+    await user.collection.drop();
+  } catch (e) {
+    if (e.code === 26) {
+      console.log('namespace %s not found', user.collection.name)
+    } else {
+      /* Todo return error */
+      throw e;
+    }
+  } finally {
+    res.send('Users collection successfully dropped!');
+  }
 });
 
 module.exports = router;
